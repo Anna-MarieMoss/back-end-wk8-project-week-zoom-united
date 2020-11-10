f8b41ae708140bc178d3bdf9ff05aab998b65e68
Luke Fantom <luke.fantom@gmail.com>
2020-11-10 19:07:17 +0000

    update

--- db/scripts/populate_table.js
+++ db/scripts/populate_table.js
@@ -1,8 +1,8 @@
 const { query } = require("../index");
-const mentorData = require("../samples_data/mentor_date")
+const { mentorData, mentorData2 } = require("../samples_data/mentor_date");
 
 // const sqlStatement = `
-//     INSERT INTO quotes_table (quote) 
+//     INSERT INTO quotes_table (quote)
 //     VALUES ($1) RETURNING *;
 //     `;
 
@@ -11,13 +11,21 @@ const sqlStatement = `
     VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9) RETURNING *;
     `;
 
-  
-
 async function populateTable(array) {
   for (let i = 0; i < array.length; i++) {
-    let values = [array[i].user_id, array[i].name, array[i].meeting_date, array[i].week_topic, array[i].summary, array[i].challenges, array[i].wins, array[i].goals, array[i].aspirations];
+    let values = [
+      array[i].user_id,
+      array[i].name,
+      array[i].meeting_date,
+      array[i].week_topic,
+      array[i].summary,
+      array[i].challenges,
+      array[i].wins,
+      array[i].goals,
+      array[i].aspirations,
+    ];
     const result = await query(sqlStatement, values);
     console.log(result);
   }
 }
-populateTable(mentorData);
+populateTable(mentorData2);
--- routes/notes.js
+++ routes/notes.js
@@ -6,13 +6,21 @@ const {
   getNotesUser,
   getAllNotesDate,
   getAllNotesDateRange,
-  getUserNotesDate,
   getUserNotesDateRange,
 } = require("../db/models/notes");
 
 ///////// GET ALL NOTES
 
 router.get("/", async function (req, res) {
+  // If a date is passed through in params - return all notes between date given and current date
+  if (req.query.date) {
+    const notes = await getAllNotesDate(req.query.date);
+    console.log(`Success! Here's all notes since ${req.query.date}`);
+    res.json({ payload: notes });
+    return;
+  }
+
+  // If no date is passed in params return all notes
   const allNotes = await getNotesAll();
   console.log(`Success! Here's the notes`);
   res.json({ payload: allNotes });
@@ -21,17 +29,19 @@ router.get("/", async function (req, res) {
 //// GET ALL NOTES FROM USER
 
 router.get("/:id", async function (req, res) {
+  // If a date is passed through in params - return all notes between date given and current date for specified user
+  if (req.query.date) {
+    const notes = await getUserNotesDateRange(req.query.date, req.params.id);
+    console.log(
+      `Success! Here's all notes since ${req.query.date} for User: ${req.params.id}`
+    );
+    res.json({ payload: notes });
+    return;
+  }
+  // If no date is passed in params return all notes for specified user
   const userNotes = await getNotesUser(req.params.id);
   console.log(`Success! Here's the notes for User: ${req.params.id}`);
   res.json({ payload: userNotes });
 });
-
-// //// GET ALL NOTES FROM SPECIFIC DATE
-
-// router.get("/", async function (req, res) {
-//   const userNotes = await getNotesUser(req.params.id);
-//   console.log(`Success! Here's the notes for User: ${req.params.id}`);
-//   res.json({ payload: userNotes });
-// });
 
 module.exports = router;
