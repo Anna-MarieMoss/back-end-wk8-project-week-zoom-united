acd30cc2850607ca46f28daf150da68c31acca3b
Anna-Marie Moss <ambrown1976@googlemail.com>
2020-11-16 16:07:12 +0000

    db credential change

--- db/index.js
+++ db/index.js
@@ -2,11 +2,12 @@
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  user: process.env.PGUSER,
-  host: process.env.PGHOST,
-  database: process.env.PGDATABASE,
-  password: process.env.PGPASSWORD,
-  port: 5432,
+  // user: process.env.PGUSER,
+  // host: process.env.PGHOST,
+  // database: process.env.PGDATABASE,
+  // password: process.env.PGPASSWORD,
+  // port: 5432,
+  connectionString: process.env.DATABASE_URL,
   ssl: { rejectUnauthorized: false },
 });
 
