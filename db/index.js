d4cc147f81094ab35afa21254c8d93efc228c040
Luke Fantom <luke.fantom@gmail.com>
2020-11-17 13:57:16 +0000

    env for deploy

--- db/index.js
+++ db/index.js
@@ -1,12 +1,7 @@
-
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  user: process.env.PGUSER,
-  host: process.env.PGHOST,
-  database: process.env.PGDATABASE,
-  password: process.env.PGPASSWORD,
-  port: 5432,
+  connectionString: process.env.DATABASE_URL,
   ssl: { rejectUnauthorized: false },
 });
 
@@ -17,6 +12,3 @@ module.exports = {
     return pool.query(text, params, callback);
   },
 };
-
-
-
